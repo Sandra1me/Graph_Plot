d5f2cc50d8b0701fc2eb7e39deebc5aae721a5ed
Sandra <82436702+Sandra1me@users.noreply.github.com>
2025-01-08 13:11:44 +0100

    Add files via upload

--- GraphPlot_Code.m
+++ GraphPlot_Code.m
@@ -18,18 +18,14 @@ classdef GraphPlot_Code < matlab.apps.AppBase
         xminSpinnerLabel                matlab.ui.control.Label
         UIAxes                          matlab.ui.control.UIAxes
         DPlotTab_2                      matlab.ui.container.Tab
-        ymaxSpinner                     matlab.ui.control.Spinner
-        ymaxSpinnerLabel                matlab.ui.control.Label
-        yminSpinner                     matlab.ui.control.Spinner
-        yminSpinnerLabel                matlab.ui.control.Label
         CustomFunctionEditField_2       matlab.ui.control.EditField
         CustomFunctionEditField_2Label  matlab.ui.control.Label
         DFunctionDropDown_2             matlab.ui.control.DropDown
         DFunctionLabel                  matlab.ui.control.Label
-        xmaxSpinner_2                   matlab.ui.control.Spinner
-        xmaxSpinner_2Label              matlab.ui.control.Label
-        xminSpinner_2                   matlab.ui.control.Spinner
-        xminSpinner_2Label              matlab.ui.control.Label
+        xmaxymaxSpinner                 matlab.ui.control.Spinner
+        xmaxymaxSpinnerLabel            matlab.ui.control.Label
+        xminyminSpinner                 matlab.ui.control.Spinner
+        xminyminSpinnerLabel            matlab.ui.control.Label
         Label_2                         matlab.ui.control.Label
         ClearButton_2                   matlab.ui.control.Button
         PlotButton_2                    matlab.ui.control.Button
@@ -87,15 +83,11 @@ classdef GraphPlot_Code < matlab.apps.AppBase
 
         % Button pushed function: PlotButton_2
         function PlotButton_2Pushed(app, event)
-            if app.xminSpinner_2.Value<app.xmaxSpinner_2.Value
-                x=linspace(app.xminSpinner_2.Value,app.xmaxSpinner_2.Value);
+            if app.xminyminSpinner.Value<app.xmaxymaxSpinner.Value
+                x=linspace(app.xminyminSpinner.Value,app.xmaxymaxSpinner.Value);
+                y=linspace(app.xminyminSpinner.Value,app.xmaxymaxSpinner.Value);
             else
                 x=linspace(-10,10);
-            end
-            
-            if app.yminSpinner.Value<app.ymaxSpinner.Value
-                x=linspace(app.yminSpinner.Value,app.ymaxSpinner.Value);
-            else
                 y=linspace(-10,10);
             end
             
@@ -170,20 +162,20 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.UIAxes.XGrid = 'on';
             app.UIAxes.YGrid = 'on';
             app.UIAxes.ButtonDownFcn = createCallbackFcn(app, @PlotButtonPushed, true);
-            app.UIAxes.Position = [203 120 414 319];
+            app.UIAxes.Position = [203 129 414 319];
 
             % Create xminSpinnerLabel
             app.xminSpinnerLabel = uilabel(app.DPlotTab);
             app.xminSpinnerLabel.HorizontalAlignment = 'right';
             app.xminSpinnerLabel.FontColor = [0.0431 0.0392 0.051];
-            app.xminSpinnerLabel.Position = [39 378 31 22];
+            app.xminSpinnerLabel.Position = [39 387 31 22];
             app.xminSpinnerLabel.Text = 'xmin';
 
             % Create DFunctionDropDownLabel
             app.DFunctionDropDownLabel = uilabel(app.DPlotTab);
             app.DFunctionDropDownLabel.HorizontalAlignment = 'right';
             app.DFunctionDropDownLabel.FontColor = [0.0431 0.0392 0.051];
-            app.DFunctionDropDownLabel.Position = [19 131 70 22];
+            app.DFunctionDropDownLabel.Position = [19 140 70 22];
             app.DFunctionDropDownLabel.Text = '2D Function';
 
             % Create PlotButton
@@ -191,7 +183,7 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.PlotButton.ButtonPushedFcn = createCallbackFcn(app, @PlotButtonPushed, true);
             app.PlotButton.BackgroundColor = [0.949 0.7451 0.1333];
             app.PlotButton.FontColor = [0.0431 0.0392 0.051];
-            app.PlotButton.Position = [360 78 100 22];
+            app.PlotButton.Position = [360 87 100 22];
             app.PlotButton.Text = 'Plot';
 
             % Create DFunctionDropDown
@@ -199,33 +191,33 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.DFunctionDropDown.Items = {'sin(x)', 'cos(x)', 'tan(x)', 'Custom'};
             app.DFunctionDropDown.ValueChangedFcn = createCallbackFcn(app, @DFunctionDropDownValueChanged, true);
             app.DFunctionDropDown.FontColor = [0.0431 0.0392 0.051];
-            app.DFunctionDropDown.Position = [104 131 100 22];
+            app.DFunctionDropDown.Position = [104 140 100 22];
             app.DFunctionDropDown.Value = 'sin(x)';
 
             % Create xminSpinner
             app.xminSpinner = uispinner(app.DPlotTab);
             app.xminSpinner.FontColor = [0.0431 0.0392 0.051];
-            app.xminSpinner.Position = [83 378 100 22];
+            app.xminSpinner.Position = [83 387 100 22];
 
             % Create CustomFunctionEditFieldLabel
             app.CustomFunctionEditFieldLabel = uilabel(app.DPlotTab);
             app.CustomFunctionEditFieldLabel.HorizontalAlignment = 'right';
             app.CustomFunctionEditFieldLabel.FontColor = [0.0431 0.0392 0.051];
             app.CustomFunctionEditFieldLabel.Visible = 'off';
-            app.CustomFunctionEditFieldLabel.Position = [19 89 96 22];
+            app.CustomFunctionEditFieldLabel.Position = [19 98 96 22];
             app.CustomFunctionEditFieldLabel.Text = 'Custom Function';
 
             % Create CustomFunctionEditField
             app.CustomFunctionEditField = uieditfield(app.DPlotTab, 'text');
             app.CustomFunctionEditField.FontColor = [0.0431 0.0392 0.051];
             app.CustomFunctionEditField.Visible = 'off';
-            app.CustomFunctionEditField.Position = [130 89 100 22];
+            app.CustomFunctionEditField.Position = [130 98 100 22];
             app.CustomFunctionEditField.Value = 'x.*sin(x)';
 
             % Create xmaxSpinnerLabel
             app.xmaxSpinnerLabel = uilabel(app.DPlotTab);
             app.xmaxSpinnerLabel.HorizontalAlignment = 'right';
-            app.xmaxSpinnerLabel.Position = [36 328 34 22];
+            app.xmaxSpinnerLabel.Position = [36 337 34 22];
             app.xmaxSpinnerLabel.Text = 'xmax';
 
             % Create ClearButton
@@ -233,20 +225,20 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.ClearButton.ButtonPushedFcn = createCallbackFcn(app, @ClearButtonPushed, true);
             app.ClearButton.BackgroundColor = [0.949 0.6627 0.1333];
             app.ClearButton.FontColor = [0.0431 0.0392 0.051];
-            app.ClearButton.Position = [360 36 100 22];
+            app.ClearButton.Position = [360 45 100 22];
             app.ClearButton.Text = 'Clear';
 
             % Create xmaxSpinner
             app.xmaxSpinner = uispinner(app.DPlotTab);
             app.xmaxSpinner.FontColor = [0.0431 0.0392 0.051];
-            app.xmaxSpinner.Position = [82 328 100 22];
+            app.xmaxSpinner.Position = [82 337 100 22];
 
             % Create Label
             app.Label = uilabel(app.DPlotTab);
             app.Label.HorizontalAlignment = 'center';
             app.Label.WordWrap = 'on';
             app.Label.FontColor = [0.0431 0.0392 0.051];
-            app.Label.Position = [37 178 145 112];
+            app.Label.Position = [37 187 145 112];
             app.Label.Text = {'WARNING!'; 'If you write a custom function, it is important to use the dot notation. '; 'Examples:'; 'x.*sin(x)'; 'x.^2'; '2*sin(x)'};
 
             % Create DPlotTab_2
@@ -261,14 +253,14 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             zlabel(app.UIAxes_2, 'Z')
             app.UIAxes_2.XGrid = 'on';
             app.UIAxes_2.YGrid = 'on';
-            app.UIAxes_2.Position = [203 120 414 319];
+            app.UIAxes_2.Position = [212 116 414 319];
 
             % Create PlotButton_2
             app.PlotButton_2 = uibutton(app.DPlotTab_2, 'push');
             app.PlotButton_2.ButtonPushedFcn = createCallbackFcn(app, @PlotButton_2Pushed, true);
             app.PlotButton_2.BackgroundColor = [0.949 0.7451 0.1333];
             app.PlotButton_2.FontColor = [0.0431 0.0392 0.051];
-            app.PlotButton_2.Position = [398 78 100 22];
+            app.PlotButton_2.Position = [407 74 100 22];
             app.PlotButton_2.Text = 'Plot';
 
             % Create ClearButton_2
@@ -276,7 +268,7 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.ClearButton_2.ButtonPushedFcn = createCallbackFcn(app, @ClearButton_2Pushed, true);
             app.ClearButton_2.BackgroundColor = [0.949 0.6627 0.1333];
             app.ClearButton_2.FontColor = [0.0431 0.0392 0.051];
-            app.ClearButton_2.Position = [398 36 100 22];
+            app.ClearButton_2.Position = [407 32 100 22];
             app.ClearButton_2.Text = 'Clear';
 
             % Create Label_2
@@ -284,38 +276,38 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.Label_2.HorizontalAlignment = 'center';
             app.Label_2.WordWrap = 'on';
             app.Label_2.FontColor = [0.0431 0.0392 0.051];
-            app.Label_2.Position = [39 134 145 112];
+            app.Label_2.Position = [28 174 145 112];
             app.Label_2.Text = {'WARNING!'; 'If you write a custom function, it is important to use the dot notation. '; 'Examples:'; 'x.*sin(x)'; 'x.^2'; '2*sin(x)'};
 
-            % Create xminSpinner_2Label
-            app.xminSpinner_2Label = uilabel(app.DPlotTab_2);
-            app.xminSpinner_2Label.HorizontalAlignment = 'right';
-            app.xminSpinner_2Label.FontColor = [0.0431 0.0392 0.051];
-            app.xminSpinner_2Label.Position = [37 389 31 22];
-            app.xminSpinner_2Label.Text = 'xmin';
+            % Create xminyminSpinnerLabel
+            app.xminyminSpinnerLabel = uilabel(app.DPlotTab_2);
+            app.xminyminSpinnerLabel.HorizontalAlignment = 'right';
+            app.xminyminSpinnerLabel.FontColor = [0.0431 0.0392 0.051];
+            app.xminyminSpinnerLabel.Position = [14 385 63 22];
+            app.xminyminSpinnerLabel.Text = 'xmin, ymin';
 
-            % Create xminSpinner_2
-            app.xminSpinner_2 = uispinner(app.DPlotTab_2);
-            app.xminSpinner_2.FontColor = [0.0431 0.0392 0.051];
-            app.xminSpinner_2.Position = [83 389 100 22];
+            % Create xminyminSpinner
+            app.xminyminSpinner = uispinner(app.DPlotTab_2);
+            app.xminyminSpinner.FontColor = [0.0431 0.0392 0.051];
+            app.xminyminSpinner.Position = [92 385 100 22];
 
-            % Create xmaxSpinner_2Label
-            app.xmaxSpinner_2Label = uilabel(app.DPlotTab_2);
-            app.xmaxSpinner_2Label.HorizontalAlignment = 'right';
-            app.xmaxSpinner_2Label.FontColor = [0.0431 0.0392 0.051];
-            app.xmaxSpinner_2Label.Position = [37 348 34 22];
-            app.xmaxSpinner_2Label.Text = 'xmax';
+            % Create xmaxymaxSpinnerLabel
+            app.xmaxymaxSpinnerLabel = uilabel(app.DPlotTab_2);
+            app.xmaxymaxSpinnerLabel.HorizontalAlignment = 'right';
+            app.xmaxymaxSpinnerLabel.FontColor = [0.0431 0.0392 0.051];
+            app.xmaxymaxSpinnerLabel.Position = [10 344 70 22];
+            app.xmaxymaxSpinnerLabel.Text = 'xmax, ymax';
 
-            % Create xmaxSpinner_2
-            app.xmaxSpinner_2 = uispinner(app.DPlotTab_2);
-            app.xmaxSpinner_2.FontColor = [0.0431 0.0392 0.051];
-            app.xmaxSpinner_2.Position = [83 348 100 22];
+            % Create xmaxymaxSpinner
+            app.xmaxymaxSpinner = uispinner(app.DPlotTab_2);
+            app.xmaxymaxSpinner.FontColor = [0.0431 0.0392 0.051];
+            app.xmaxymaxSpinner.Position = [92 344 100 22];
 
             % Create DFunctionLabel
             app.DFunctionLabel = uilabel(app.DPlotTab_2);
             app.DFunctionLabel.HorizontalAlignment = 'right';
             app.DFunctionLabel.FontColor = [0.0431 0.0392 0.051];
-            app.DFunctionLabel.Position = [19 78 70 22];
+            app.DFunctionLabel.Position = [28 74 70 22];
             app.DFunctionLabel.Text = '3D Function';
 
             % Create DFunctionDropDown_2
@@ -323,7 +315,7 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.DFunctionDropDown_2.Items = {'sin(x)+cos(y)', 'x.*sin(y)', 'x.^2+y.^2', 'Custom'};
             app.DFunctionDropDown_2.ValueChangedFcn = createCallbackFcn(app, @DFunctionDropDown_2ValueChanged, true);
             app.DFunctionDropDown_2.FontColor = [0.0431 0.0392 0.051];
-            app.DFunctionDropDown_2.Position = [104 78 112 22];
+            app.DFunctionDropDown_2.Position = [113 74 112 22];
             app.DFunctionDropDown_2.Value = 'sin(x)+cos(y)';
 
             % Create CustomFunctionEditField_2Label
@@ -331,39 +323,15 @@ classdef GraphPlot_Code < matlab.apps.AppBase
             app.CustomFunctionEditField_2Label.HorizontalAlignment = 'right';
             app.CustomFunctionEditField_2Label.FontColor = [0.0431 0.0392 0.051];
             app.CustomFunctionEditField_2Label.Visible = 'off';
-            app.CustomFunctionEditField_2Label.Position = [19 36 96 22];
+            app.CustomFunctionEditField_2Label.Position = [28 32 96 22];
             app.CustomFunctionEditField_2Label.Text = 'Custom Function';
 
             % Create CustomFunctionEditField_2
             app.CustomFunctionEditField_2 = uieditfield(app.DPlotTab_2, 'text');
             app.CustomFunctionEditField_2.FontColor = [0.0431 0.0392 0.051];
             app.CustomFunctionEditField_2.Visible = 'off';
-            app.CustomFunctionEditField_2.Position = [130 36 100 22];
+            app.CustomFunctionEditField_2.Position = [139 32 100 22];
             app.CustomFunctionEditField_2.Value = 'y.*sin(x)';
-
-            % Create yminSpinnerLabel
-            app.yminSpinnerLabel = uilabel(app.DPlotTab_2);
-            app.yminSpinnerLabel.HorizontalAlignment = 'right';
-            app.yminSpinnerLabel.FontColor = [0.0431 0.0392 0.051];
-            app.yminSpinnerLabel.Position = [37 307 31 22];
-            app.yminSpinnerLabel.Text = 'ymin';
-
-            % Create yminSpinner
-            app.yminSpinner = uispinner(app.DPlotTab_2);
-            app.yminSpinner.FontColor = [0.0431 0.0392 0.051];
-            app.yminSpinner.Position = [83 307 100 22];
-
-            % Create ymaxSpinnerLabel
-            app.ymaxSpinnerLabel = uilabel(app.DPlotTab_2);
-            app.ymaxSpinnerLabel.HorizontalAlignment = 'right';
-            app.ymaxSpinnerLabel.FontColor = [0.0431 0.0392 0.051];
-            app.ymaxSpinnerLabel.Position = [37 268 34 22];
-            app.ymaxSpinnerLabel.Text = 'ymax';
-
-            % Create ymaxSpinner
-            app.ymaxSpinner = uispinner(app.DPlotTab_2);
-            app.ymaxSpinner.FontColor = [0.0431 0.0392 0.051];
-            app.ymaxSpinner.Position = [83 268 100 22];
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
